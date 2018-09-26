1e8074af0d86d8ff69c29d38b83466e100d91283
nju33 <nju33.ki@gmail.com>
2018-09-26 18:33:39 +0900

    fix: types

--- src/with-dou.tsx
+++ src/with-dou.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import {FunctionsContext, DouFunctionsContext} from './functions-context';
+import {
+  FunctionsContext /* , DouFunctionsContext */,
+} from './functions-context';
 
-function withDou<P = {}, S = any>(
-  Component:
-    | React.ComponentClass<P & {dou: DouFunctionsContext}, S>
-    | React.SFC<P & {dou: DouFunctionsContext}>,
+function withDou<P, S>(
+  Component: React.ComponentClass<P, S> | React.SFC<P>,
 ) {
-  return class extends React.Component<P & {dou: DouFunctionsContext}, S> {
+  return class extends React.Component<P, S> {
+    static displayName = `withDou(${Component.displayName})`;
+
     render() {
       return (
         <FunctionsContext.Consumer>
           {dou => {
+            // @ts-ignore
             return <Component {...this.props} dou={dou} />;
           }}
         </FunctionsContext.Consumer>
